df88d8918f5e62ba2b09f04bc77b5c304194238c
wony3013 <dnjs3013@naver.com>
2022-06-19 23:34:08 +0900

    상품 조회 기능 추가

---
+++ src/main/java/app/CsvFileHandler.java
@@ -0,0 +1,31 @@
+package app;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CsvFileHandler {
+
+	Logger logger = LoggerFactory.getLogger(CsvFileHandler.class);
+
+	public List<String[]> getItemListArray(){
+		File itemListCsvFile = new File("src/main/resources/snack_list.csv");
+
+		List<String[]> itemList = new ArrayList<>();
+
+		try(Scanner is = new Scanner(itemListCsvFile)){
+			while (is.hasNext()){
+				String[] itemValue = is.next().split(",");
+				itemList.add(itemValue);
+			}
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+		return itemList;
+	}
+}
---
+++ src/main/java/app/Products.java
@@ -0,0 +1,29 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Products {
+
+	List<Item> itemList = new ArrayList<Item>();
+
+	public Products(){
+		CsvFileHandler csvFileHandler = new CsvFileHandler();
+		csvFileHandler.getItemListArray().stream().forEach(strings -> {
+			this.itemList.add(new Item(strings[0], strings[1], Integer.parseInt(strings[2])));
+		});
+	}
+
+	public void addItem(String id, String name, int price){
+		this.itemList.add(new Item(id, name, price));
+	}
+
+	public void removeItem(String targetId){
+		this.itemList.removeIf(item -> item.id.equals(targetId));
+	}
+
+	public List<Item> getItem(){
+		return this.itemList;
+	}
+
+}
