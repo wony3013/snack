a41c04a032d5b879f2bc013c5f4aab96825bd421
wony3013 <dnjs3013@naver.com>
2022-06-27 02:01:19 +0900

    상품등록시 mem.에 상품등록 될 수 있도록 로직 수정

--- src/main/java/server/RequestHandler.java
+++ src/main/java/server/RequestHandler.java
@@ -5,7 +5,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.http.HttpRequest;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class RequestHandler extends Thread{
 	private static final Logger log =  LoggerFactory.getLogger(RequestHandler.class);
@@ -33,36 +36,64 @@ public class RequestHandler extends Thread{
 
 			String[] tokens = line.split(" ");
 			StringBuffer response = new StringBuffer();
+			int dataLength = 0;
+
 			while (!"".equals(line)){
-				line = br.readLine();
 				response.append(line);
+				if(line.contains("Content-Length")){
+					dataLength = Integer.parseInt(line.replaceAll(" ","").split(":")[1]);
+				}
 				log.debug("header : {}", line);
+				line = br.readLine();
 			}
-
-			if("/product".equals(line)){
-
-			} else if ("/product/regForm".equals(line)) {
-				log.debug("======================");
-				log.debug(response.toString());
-				log.debug("======================");
+			byte[] body = Files.readAllBytes(new File("./src/webapp/index.html").toPath());
+			if("/product".equals(tokens[1])){
+				log.debug("상품 등록 폼");
+				body = Files.readAllBytes(new File("./src/webapp" + tokens[1]+".html").toPath());
+			} else if ("/product/regForm".equals(tokens[1])) {
+				// TODO 상품 등록 된 후 Return 시켜줄 html 생성하여 이곳 작성
+			}
+			int requestBodyLength;
+			HashMap<String, String> request = null;
+			if(dataLength > 0) {
+				char[] cubf = getCubf(dataLength, br);
+				request = makeRequest(cubf);
+				log.debug("request parameter : {}", request.toString());
 			}
 
 
 			File index = new File("./src/webapp/index.html");
+
+
+
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "SNACK WORLD".getBytes();
-			byte[] body2 = Files.readAllBytes(index.toPath());
-
-			byte[] body3 = Files.readAllBytes(new File("./src/webapp" + tokens[1]).toPath());
-
-			response200Header(dos, body3.length);
-			responseBody(dos,body3);
+			response200Header(dos, body.length);
+			responseBody(dos,body);
 
 		}catch (IOException e){
 			log.error(e.getMessage());
 
 		}
 
+	}
+
+	public char[] getCubf(int dataLength, BufferedReader br) throws IOException {
+		char[] cubf = null;
+		cubf = new char[dataLength];
+		int requestBodyLength = br.read(cubf);
+		return cubf;
+	}
+
+
+	public HashMap<String, String> makeRequest(char[] cubf){
+		HashMap<String, String> request = new HashMap<>();
+		String requestString = new String(cubf);
+		String[] splitedRequestString = requestString.split("&");
+		Arrays.stream(splitedRequestString).forEach(s -> {
+			String[] bb = s.split("=");
+			request.put(bb[0], bb[1]);
+		});
+		return request;
 	}
 
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent){
