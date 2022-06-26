509e66974ae20716b4a99951fd9f403b53bf6651
wony3013 <dnjs3013@naver.com>
2022-06-27 02:01:35 +0900

    상품등록 관련 기능 테스트 추가

---
+++ src/test/java/RequestHandlerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import server.RequestHandler;
+
+public class RequestHandlerTest {
+
+	static Logger log = LoggerFactory.getLogger(RequestHandlerTest.class);
+
+	@Test
+	void makeRequest(){
+		RequestHandler requestHandler;
+		// char[] chub = new char[];
+		// requestHandler.makeRequest();
+	}
+}
--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -25,5 +27,24 @@ public class TestCase {
 		CsvFileHandler csvFileHandler = new CsvFileHandler();
 		List<String[]> itemList = csvFileHandler.getItemListArray();
 		itemList.stream().forEach(strings -> logger.debug(strings[1]));
+	}
+
+	@Test
+	public void split(){
+		String a = "id=a1&name=b2&price=c33";
+		String[] b = a.split("&");
+
+		HashMap<String, String> request = new HashMap<>();
+
+		Arrays.stream(b).forEach(s -> {
+			String[] bb = s.split("=");
+			request.put(bb[0], bb[1]);
+		});
+
+		logger.debug(request.get("id"));
+		logger.debug(request.get("name"));
+		logger.debug(request.get("price"));
+
+
 	}
 }
